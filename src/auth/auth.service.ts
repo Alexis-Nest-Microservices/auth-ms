3819a4175741ea5911ff4845f517b71e3d1a842e
alexis.rueda <alexisrueda34@gmail.com>
2025-09-28 00:37:32 -0600

    Agregar implementación del método de inicio de sesión en el servicio de autenticación, incluyendo validación de usuario y contraseña.

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -4,7 +4,7 @@ import { RpcException } from '@nestjs/microservices';
 
 import * as bcrypt from 'bcrypt';
 
-import { RegisterUserDto } from './dto';
+import { LoginUserDto, RegisterUserDto } from './dto';
 
 @Injectable()
 export class AuthService extends PrismaClient implements OnModuleInit {
@@ -42,6 +42,44 @@ export class AuthService extends PrismaClient implements OnModuleInit {
       });
 
       const { password: __, ...rest } = newUser;
+
+      return {
+        user: rest,
+        token: 'fake-jwt-token',
+      };
+    } catch (error) {
+      throw new RpcException({
+        status: 400,
+        message: error.message,
+      });
+    }
+  }
+
+  async loginUser(loginUserDto: LoginUserDto) {
+    const { email, password } = loginUserDto;
+
+    try {
+      const user = await this.user.findUnique({
+        where: { email },
+      });
+
+      if (!user) {
+        throw new RpcException({
+          status: 400,
+          message: 'User/Password not valid',
+        });
+      }
+
+      const isPasswordValid = bcrypt.compareSync(password, user.password);
+
+      if (!isPasswordValid) {
+        throw new RpcException({
+          status: 400,
+          message: 'User/Password not valid',
+        });
+      }
+
+      const { password: __, ...rest } = user;
 
       return {
         user: rest,
